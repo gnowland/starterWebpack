fc4eb2ff3d4bd9011b1f2f921868a58a8040a6ad
Gifford Nowland <hi@giffordnowland.com>
2018-11-03 13:23:40 -0700

    Remove sweet-alert.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,6 @@
-/* global document sweetAlert */
-
 import app from './app';
-import './sweet-alert';
 
 document.addEventListener('DOMContentLoaded', () => {
   const myConst = 'test ES6 transpiling';
   app.init(); // testing my custom module
-  sweetAlert('Hello world!'); // testing old skool external plugin import
 });
