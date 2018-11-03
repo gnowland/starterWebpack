8d44d050e01fd82dee385ab293dd49225a2bd4a5
Gifford Nowland <hi@giffordnowland.com>
2018-11-03 13:23:08 -0700

    remove jQuery

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,10 @@
 /* global document sweetAlert */
 
-import $ from 'jquery';
 import app from './app';
 import './sweet-alert';
 
 document.addEventListener('DOMContentLoaded', () => {
   const myConst = 'test ES6 transpiling';
   app.init(); // testing my custom module
-  console.log($, myConst); // testing jQuery and transpiling
   sweetAlert('Hello world!'); // testing old skool external plugin import
 });
